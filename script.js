dc2b6dfd518ffe728fb22302d7d08fd093915055
Mehedi Hasan Shojib <125130261+Ahamed-Shojib@users.noreply.github.com>
2023-10-28 17:48:39 +0600

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-let cart = [];
+// Generate a random number between 1 and 100
+const targetNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 10;
 
-        function addToCart(product, price) {
-            cart.push({ product, price });
-            updateCart();
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const message = document.getElementById("message");
+const attemptsSpan = document.getElementById("attempts");
+
+// Function to handle the player's guess
+guessButton.addEventListener("click", function () {
+    const guess = parseInt(guessInput.value);
+
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+        message.innerHTML = `<p style="font-weight:800;color:red">Please enter a valid number between 1 and 100.</p>`;
+    } else {
+        attempts--;
+        attemptsSpan.textContent = attempts;
+
+        if (guess === targetNumber) {
+            message.innerHTML = `<div style="font-weight:800;" class="text-success">Congratulations! You guessed the number ${targetNumber} correctly!</div>`;
+            guessButton.disabled = true;
+        } else if (attempts === 0) {
+            message.innerHTML = `<div style="font-weight:800;" class="text-warning">Sorry, you've run out of attempts. The correct number was</div><label  style="font-weight:900;color:blue;">(${targetNumber})</label>`;
+            guessButton.disabled = true;
+        } else {
+            const highOrLow =
+                guess < targetNumber ? "too low" : "too high";
+            message.innerHTML = `<div style="font-weight:800;" class="text-danger">Wrong guess! Your guess is ${highOrLow}. Try again.</div>`;
         }
-
-        function removeFromCart(index) {
-            cart.splice(index, 1);
-            updateCart();
-        }
-
-        function updateCart() {
-            const cartList = document.getElementById("cart");
-            const totalElement = document.getElementById("total");
-            let total = 0;
-
-            cartList.innerHTML = ''; // Clear the cart list
-
-            cart.forEach((item, index) => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `${item.product} - $${item.price} <button class="btn btn-sm btn-outline-danger my-1 mx-3" onclick="removeFromCart(${index})">Remove</button>`;
-                cartList.appendChild(listItem);
-                total += item.price;
-            });
-
-            totalElement.textContent = total;
-        }
+    }
+});
